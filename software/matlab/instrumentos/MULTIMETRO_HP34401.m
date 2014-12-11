c5e7c42288fc70fede12284ac575d1e70ac3fca3
diegoshalom <diegoshalom@gmail.com>
2014-12-11 16:46:56 -0300

    Mi primer commit

---
+++ software/matlab/instrumentos/MULTIMETRO_HP34401.m
@@ -0,0 +1,32 @@
+%% Multimetro HP 34401
+
+disp('Multimetro HP 34401')
+disp('MANUAL: http://www.physics.rutgers.edu/ugrad/327/HP34401.pdf')
+
+
+%reconoce al multimetro
+vu = visa('ni','GPIB1::22::INSTR');
+
+%abre la sesión Visa de comunicación con el multimetro
+fopen(m);
+
+%pide voltaje dc
+str=query(mm,'measure:voltage:DC?');
+
+%otras cosas que se pueden medir
+% MEASure
+%  :VOLTage:DC? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
+%  :VOLTage:DC:RATio? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
+%  :VOLTage:AC? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
+%  :CURRent:DC? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
+%  :CURRent:AC? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
+%  :RESistance? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
+%  :FRESistance? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
+%  :FREQuency? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
+%  :PERiod? {<range>|MIN|MAX|DEF},{<resolution>|MIN|MAX|DEF}
+%  :CONTinuity?
+%  :DIODe?
+
+
+%cierra la sesión con mm
+fclose(mm);
