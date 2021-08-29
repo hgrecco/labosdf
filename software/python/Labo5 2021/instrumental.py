6f9d3ee1505f7d07a9f2f818dfc086b14d70c949
diegoshalom <diegoshalom@gmail.com>
2021-08-29 15:07:12 -0300

    Arreglo tabulacion de comentario

--- software/python/Labo5 2021/instrumental.py
+++ software/python/Labo5 2021/instrumental.py
@@ -3,39 +3,39 @@ import numpy as np
 import time
 
 class HantekPPS2320A:
-"""
-Fuente DC
-Send Command Word	Perform Operation
-a + line break (Hereafter, every command must take 0x0a as the line break to over, ignore the following)	Back to device model
-suXXXX	CH1 preset output voltage, units V; e.g. 1200 stands for 12.00V
-siXXXX 	CH1 preset output current, units A; e.g. 2500 stands for 2.500A
-saXXXX	CH2 preset output voltage, units V; e.g. 1200 stands for 12.00V
-sdXXXX	CH2 preset output current, units A; e.g. 2500 stands for 2.500A
-O0	Output indicator light switch-off
-O1	Output indicator light switch-on
-O2	Parallel, series, trace, output indicator light switch-off
-O3	Series, trace, output indicator switch-off; Parallel indicator light switch-on
-O4	Parallel, trace, output indicator switch-off; Series indicator light switch-on
-O5	Parallel, series, output indicator switch-off; Trace indicator light switch-on
-O6	CH1 indicator light switch-on
-O7	CH2 indicator light switch-on
-O8	CH3 3.3V indicator light switch-on
-O9	CH3 5V indicator light switch-on
-Oa	CH3 2.5V indicator light switch-on
-rv	Read the measured voltage of CH1
-ra	Read the measured current of CH1
-ru	Read the preset voltage of CH1
-ri	Read the preset current of CH1
-rh	Read the measured voltage of CH2
-rj	Read the measured current of CH2
-rk	Read the preset voltage of CH2
-rq	Read the preset current of CH2
-rm	Read the device working mode
-rl	Read lock state
-rp	Read CH2 state
-rs	Read CH1 state
-rb	Read CH3 state	
-"""    
+    """
+    Fuente DC
+    Send Command Word	Perform Operation
+    a + line break (Hereafter, every command must take 0x0a as the line break to over, ignore the following)	Back to device model
+    suXXXX	CH1 preset output voltage, units V; e.g. 1200 stands for 12.00V
+    siXXXX 	CH1 preset output current, units A; e.g. 2500 stands for 2.500A
+    saXXXX	CH2 preset output voltage, units V; e.g. 1200 stands for 12.00V
+    sdXXXX	CH2 preset output current, units A; e.g. 2500 stands for 2.500A
+    O0	Output indicator light switch-off
+    O1	Output indicator light switch-on
+    O2	Parallel, series, trace, output indicator light switch-off
+    O3	Series, trace, output indicator switch-off; Parallel indicator light switch-on
+    O4	Parallel, trace, output indicator switch-off; Series indicator light switch-on
+    O5	Parallel, series, output indicator switch-off; Trace indicator light switch-on
+    O6	CH1 indicator light switch-on
+    O7	CH2 indicator light switch-on
+    O8	CH3 3.3V indicator light switch-on
+    O9	CH3 5V indicator light switch-on
+    Oa	CH3 2.5V indicator light switch-on
+    rv	Read the measured voltage of CH1
+    ra	Read the measured current of CH1
+    ru	Read the preset voltage of CH1
+    ri	Read the preset current of CH1
+    rh	Read the measured voltage of CH2
+    rj	Read the measured current of CH2
+    rk	Read the preset voltage of CH2
+    rq	Read the preset current of CH2
+    rm	Read the device working mode
+    rl	Read lock state
+    rp	Read CH2 state
+    rs	Read CH1 state
+    rb	Read CH3 state	
+    """    
     def __init__(self, resource):
         self._fuente = visa.ResourceManager().open_resource(resource)
         print(self._fuente.query('*IDN?'))
